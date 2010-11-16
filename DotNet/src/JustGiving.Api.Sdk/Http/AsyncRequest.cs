b7be44a6bfb9aa0338bd25478b74170abaf3e013
davidwhitney <david@davidwhitney.co.uk>
2010-11-16 13:00:18 +0000

    Added missing async request.

---
+++ DotNet/src/JustGiving.Api.Sdk/Http/AsyncRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using JustGiving.Api.Sdk.Http.DataPackets;
+
+namespace JustGiving.Api.Sdk
+{
+    public class AsyncRequest
+    {
+        public byte[] RawPostData { get; set; }
+        public string RawPostDataContentType { get; set; }
+        public Action<HttpResponseMessage> HttpClientCallback { get; set; }
+        public HttpWebRequest WebRequest { get; set; }
+        public HttpContent PostData { get; set; }
+    }
+}
