1486dfefb3b72aa0074bd16d66c5b99cb7c10961
Tim Butterfield <Tim.Butterfield@justgiving.com>
2012-08-07 10:31:12 +0100

    Moved types to own files, changed int property to nullable to support JSON deserialisation of null ints

--- DotNet/src/JustGiving.Api.Data.Sdk/Model/Payment/GiftAid/FundraisingPage.cs
+++ DotNet/src/JustGiving.Api.Data.Sdk/Model/Payment/GiftAid/FundraisingPage.cs
@@ -14,7 +14,7 @@ namespace JustGiving.Api.Data.Sdk.Model.Payment.GiftAid
         }
 
         [DataMember(Order = 10)]
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
         [DataMember(Order = 20)]
         public string Title { get; set; }
@@ -45,128 +45,5 @@ namespace JustGiving.Api.Data.Sdk.Model.Payment.GiftAid
 
         [DataMember(Order = 120)]
         public CustomCodes CustomCodes { get; set; }
-    }
-
-    [DataContract(Name = "Address", Namespace = "")]
-    public class Address
-    {
-        [DataMember(Order = 10)]
-        public string AddressLine1 { get; set; }
-
-        [DataMember(Order = 20)]
-        public string AddressLine2 { get; set; }
-
-        [DataMember(Order = 30)]
-        public string Town { get; set; }
-
-        [DataMember(Order = 40)]
-        public string Region { get; set; }
-
-        [DataMember(Order = 50)]
-        public string Postcode { get; set; }
-
-        [DataMember(Order = 60)]
-        public string Country { get; set; }
-    }
-
-    [DataContract(Name = "Fundraiser", Namespace = "")]
-    public class Fundraiser
-    {
-        public Fundraiser()
-        {
-            Address = new Address();
-        }
-
-        [DataMember(Order = 10)]
-        public int? UserId { get; set; }
-
-        [DataMember(Order = 20)]
-        public string Title { get; set; }
-
-        [DataMember(Order = 30)]
-        public string FirstName { get; set; }
-
-        [DataMember(Order = 40)]
-        public string LastName { get; set; }
-
-        [DataMember(Order = 50)]
-        public string Email { get; set; }
-
-        [DataMember(Order = 60)]
-        public Address Address { get; set; }
-
-        [DataMember(Order = 70)]
-        public bool IsFurtherContact { get; set; }
-    }
-
-    [DataContract(Name = "CustomCodes", Namespace = "")]
-    public class CustomCodes
-    {
-        [DataMember(Order = 10)]
-        public string CustomCode1 { get; set; }
-
-        [DataMember(Order = 20)]
-        public string CustomCode2 { get; set; }
-
-        [DataMember(Order = 30)]
-        public string CustomCode3 { get; set; }
-
-        [DataMember(Order = 40)]
-        public string CustomCode4 { get; set; }
-
-        [DataMember(Order = 50)]
-        public string CustomCode5 { get; set; }
-
-        [DataMember(Order = 60)]
-        public string CustomCode6 { get; set; }
-    }
-
-    [DataContract(Name = "Event", Namespace = "")]
-    public class Event
-    {
-        public Event()
-        {
-            CustomCodes = new EventCustomCodes();
-        }
-
-        [DataMember(Order = 10)]
-        public string Id { get; set; }
-
-        [DataMember(Order = 20)]
-        public string Name { get; set; }
-
-        [DataMember(Order = 30)]
-        public DateTime StartDate { get; set; }
-
-        [DataMember(Order = 40)]
-        public DateTime ExpiryDate { get; set; }
-
-        [DataMember(Order = 50)]
-        public bool IsPromoted { get; set; }
-
-        [DataMember(Order = 60)]
-        public string CategoryName { get; set; }
-
-        [DataMember(Order = 70)]
-        public bool IsOverSeas { get; set; }
-
-        [DataMember(Order = 80)]
-        public bool IsUserCreated { get; set; }
-
-        [DataMember(Order = 90)]
-        public EventCustomCodes CustomCodes { get; set; }
-    }
-
-    [DataContract(Name = "EventCustomCodes", Namespace = "")]
-    public class EventCustomCodes
-    {
-        [DataMember(Order = 10)]
-        public string CustomCode1 { get; set; }
-
-        [DataMember(Order = 20)]
-        public string CustomCode2 { get; set; }
-
-        [DataMember(Order = 30)]
-        public string CustomCode3 { get; set; }
     }
 }
