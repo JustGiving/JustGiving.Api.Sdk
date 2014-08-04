5a18fa1e6a2559647e4071566f2886ecefa7ecec
pawelsawicz <pawel.sawicz@justgiving.com>
2014-08-04 14:35:34 +0100

    cleaning

--- DotNet/src/JustGiving.Api.Sdk/ApiClients/OneSearchApi.cs
+++ DotNet/src/JustGiving.Api.Sdk/ApiClients/OneSearchApi.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
 using JustGiving.Api.Sdk.Http;
 using JustGiving.Api.Sdk.Model.OneSearch;
 
