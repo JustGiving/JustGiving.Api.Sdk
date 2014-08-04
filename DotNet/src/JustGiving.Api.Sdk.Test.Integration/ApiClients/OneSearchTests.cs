d96555d9a009832a041267d6b29478d04718ec76
pawelsawicz <pawel.sawicz@justgiving.com>
2014-08-04 14:17:35 +0100

    CAPI-237 Created new test for OneSearchTests

---
+++ DotNet/src/JustGiving.Api.Sdk.Test.Integration/ApiClients/OneSearchTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JustGiving.Api.Sdk.ApiClients;
+using NUnit.Framework;
+
+namespace JustGiving.Api.Sdk.Test.Integration.ApiClients
+{
+    public class OneSearchTests
+    {
+        private const string CorrectQuery = "cancer";
+        private const int CorrectLimit = 3;
+        private const string CorrectCountry = "GB";
+
+        [TestCase(WireDataFormat.Json)]
+        public void OneSearchIndex_KeywordWithKnownResult_SearchResultsPresent(WireDataFormat format)
+        {
+            //arrange
+            var client = TestContext.CreateClientNoCredentials(format);
+            var oneSearchClient = new OneSearchApi(client.HttpChannel);
+            if (format == WireDataFormat.Json)
+            {
+                client.HttpClient.AddHeader("Accept", "application/json");
+            }
+
+            //act
+            var result = oneSearchClient.OneSearchIndex(CorrectQuery, false, "", CorrectLimit, 0, CorrectCountry);
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(CorrectQuery, result.Query);
+            Assert.AreEqual(CorrectLimit, result.Limit);
+            Assert.AreEqual(CorrectCountry, result.Country);
+
+        }
+    }
+}
