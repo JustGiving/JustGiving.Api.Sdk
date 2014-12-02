a209c4fc3c1640f266329fdafba2f552930c4f05
pawelsawicz <pawel.sawicz@justgiving.com>
2014-12-02 04:24:51 +0100

    CAPI-376 add EventSearch into PHP SDK

--- PHP/ApiClients/SearchApi.php
+++ PHP/ApiClients/SearchApi.php
@@ -20,4 +20,12 @@ class SearchApi extends ClientBase
 		$json = $this->curlWrapper->Get($url, $this->BuildAuthenticationValue());
 		return json_decode($json); 
 	}
+
+	public function EventSearch($searchTerms, $pageSize=50, $pageNumber=1)
+	{
+		$locationFormat = $this->Parent->RootDomain . "{apiKey}/v{apiVersion}/event/search?q=".urlencode($searchTerms)."&PageSize=".$pageSize."&PageNum=".$pageNumber;
+		$url = $this->BuildUrl($locationFormat);	
+		$json = $this->curlWrapper->Get($url, $this->BuildAuthenticationValue());
+		return json_decode($json); 
+	}
 }
