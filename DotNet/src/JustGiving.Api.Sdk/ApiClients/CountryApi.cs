91e0bf919b8fd23f618332520cb530c6b5ffad5b
pawelsawicz <pawel.sawicz@justgiving.com>
2015-03-30 17:19:22 +0100

    Create country files, class and interfases

---
+++ DotNet/src/JustGiving.Api.Sdk/ApiClients/CountryApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JustGiving.Api.Sdk.Http;
+
+namespace JustGiving.Api.Sdk.ApiClients
+{
+    public class CountryApi : ApiClientBase, ICountryApi
+    {
+        public override string ResourceBase
+        {
+            get { return "{apiKey}/v{apiVersion}/countries"; }
+        }
+
+        public CountryApi(HttpChannel channel)
+            : base(channel)
+        {
+        }
+    }
+}
