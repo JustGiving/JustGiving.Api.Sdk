438c8728454848fcfb1d3c75525572c29a44fb51
pawelsawicz <pawel.sawicz@justgiving.com>
2015-03-31 17:15:05 +0100

    add currency api files

---
+++ DotNet/src/JustGiving.Api.Sdk/ApiClients/CurrencyApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JustGiving.Api.Sdk.Http;
+
+namespace JustGiving.Api.Sdk.ApiClients
+{
+    public class CurrencyApi : ApiClientBase, ICurrencyApi
+    {
+        public override string ResourceBase
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public CurrencyApi(HttpChannel channel)
+            : base(channel)
+        {
+        }
+    }
+}
