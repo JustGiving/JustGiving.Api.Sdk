882fe49b6ea2177434b5119cb092d5b13f539687
pawelsawicz <pawel.sawicz@justgiving.com>
2015-04-08 12:14:18 +0100

    create project api files and add into project

---
+++ DotNet/src/JustGiving.Api.Sdk/ApiClients/ProjectApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JustGiving.Api.Sdk.Http;
+
+namespace JustGiving.Api.Sdk.ApiClients
+{
+    public class ProjectApi : ApiClientBase, IProjectApi
+    {
+        public ProjectApi(HttpChannel channel)
+            : base(channel)
+        {
+        }
+
+        public override string ResourceBase
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
