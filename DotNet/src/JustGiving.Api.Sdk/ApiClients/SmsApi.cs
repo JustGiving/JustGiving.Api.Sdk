7b95c70e2d0733c88dfd68eaa8142e12f856a664
pawelsawicz <pawel.sawicz@justgiving.com>
2015-04-08 17:42:07 +0100

    add sms api files into project

---
+++ DotNet/src/JustGiving.Api.Sdk/ApiClients/SmsApi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JustGiving.Api.Sdk.Http;
+
+namespace JustGiving.Api.Sdk.ApiClients
+{
+    public class SmsApi : ApiClientBase, ISmsApi
+    {
+        public SmsApi(HttpChannel channel) : base(channel)
+        {
+        }
+
+        public override string ResourceBase
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
