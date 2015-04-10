3b11616aeba46677f164bf2dd8b98cf54d64c95f
pawelsawicz <pawel.sawicz@justgiving.com>
2015-04-10 14:22:33 +0100

    cleaning and refactor

--- DotNet/src/JustGiving.Api.Sdk.Test.Integration/ApiClients/TeamApiTests.cs
+++ DotNet/src/JustGiving.Api.Sdk.Test.Integration/ApiClients/TeamApiTests.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 
 using JustGiving.Api.Sdk.ApiClients;
 using JustGiving.Api.Sdk.Model.Event;
+using JustGiving.Api.Sdk.Model.Page;
 using JustGiving.Api.Sdk.Model.Team;
+using JustGiving.Api.Sdk.Test.Common.Configuration;
+using JustGiving.Api.Sdk.Test.Integration.Configuration;
 using NUnit.Framework;
 
 namespace JustGiving.Api.Sdk.Test.Integration.ApiClients
@@ -11,43 +14,32 @@ namespace JustGiving.Api.Sdk.Test.Integration.ApiClients
     [TestFixture]
     public class TeamApiTests
     {
-    	private JustGivingClient _client;
-    	private TeamApi _api;
-
-    	[SetUp]
-		public void SetUp()
-		{
-			WithAValidClient();
-		}
-
         [TestCase(WireDataFormat.Json)]
         [TestCase(WireDataFormat.Xml)]
         public void Create_ValidEvent_ReturnsEventRegistrationResponse(WireDataFormat format)
         {
-			WithAValidClient(format);
+            //arrange
+            var client = TestContext.CreateClientValidCredentials(format);
+            var teamResources = new TeamApi(client.HttpChannel);
+            var fundraiseResources = new PageApi(client.HttpChannel);
+            var validRegisterPageRequest = ValidRegisterPageRequest();
+            var validRequest = ValidTeamRequest(validRegisterPageRequest.PageShortName);
+            fundraiseResources.Create(validRegisterPageRequest);
 
-            var team = new Team
-            {
-                Name = "My Awesome event",
-                Target = 1000.00m,
-                Story = "My kick ass story",
-                TargetType = TeamTargetType.Fixed.ToString(),
-                TeamShortName = "my-great-team" + Guid.NewGuid(),
-                TeamType = TeamType.Open.ToString(),
-                TeamMembers = new List<TeamMember> { new TeamMember { PageShortName = "api-test-c166ab39-fc80-4cf1-aed0-9236974740ca" } }
-            };
-
-			var eventRegistrationResponse = _api.CreateOrUpdate(team);
-
-			Assert.That(eventRegistrationResponse.Id, Is.Not.EqualTo(0));
+            //act
+            var result = teamResources.CreateOrUpdate(validRequest);
+            
+            //assert
+            Assert.That(result.Id, Is.Not.EqualTo(0));
         }
 
         [TestCase(WireDataFormat.Json)]
         [TestCase(WireDataFormat.Xml)]
 		public void TeamExists_AndATeamExists_ReturnsTrue(WireDataFormat format)
         {
-			WithAValidClient(format);
-
+            //arrange
+            var client = TestContext.CreateClientValidCredentials(format);
+            var teamResources = new TeamApi(client.HttpChannel);
         	var team = new Team
         	             	{
         	             		Name = "My Awesome event",
@@ -59,30 +51,37 @@ namespace JustGiving.Api.Sdk.Test.Integration.ApiClients
                                 TeamMembers = new List<TeamMember> { new TeamMember { PageShortName = "api-test-c166ab39-fc80-4cf1-aed0-9236974740ca" } }
         	             	};
 
-			_api.CreateOrUpdate(team);
+            teamResources.CreateOrUpdate(team);
 
-			var existsResponse = _api.TeamExists(team.TeamShortName);
+            //act
+            var result = teamResources.TeamExists(team.TeamShortName);
 
-			Assert.That(existsResponse, Is.True);
+            //act
+            Assert.That(result, Is.True);
         }
 
         [TestCase(WireDataFormat.Json)]
         [TestCase(WireDataFormat.Xml)]
 		public void TeamExists_AndATeamDoesntExist_ReturnsFalse(WireDataFormat format)
         {
-			WithAValidClient(format);
+            //arrange
+            var client = TestContext.CreateClientValidCredentials(format);
+            var teamResources = new TeamApi(client.HttpChannel);
 			
-			var existsResponse = _api.TeamExists(Guid.NewGuid().ToString());
+            //act
+            var result = teamResources.TeamExists(Guid.NewGuid().ToString());
 
-			Assert.That(existsResponse, Is.False);
+            //act
+            Assert.That(result, Is.False);
         }
 		
 		[TestCase(WireDataFormat.Json)]
 		[TestCase(WireDataFormat.Xml)]
 		public void Retrieve_AndATeamExists_ReturnsTeam(WireDataFormat format)
 		{
-			WithAValidClient(format);
-
+            //arrange
+            var client = TestContext.CreateClientValidCredentials(format);
+            var teamResources = new TeamApi(client.HttpChannel);
 			var team1 = new Team
 			{
 				Name = "My Awesome event",
@@ -93,18 +92,47 @@ namespace JustGiving.Api.Sdk.Test.Integration.ApiClients
                 TeamType = TeamType.ByInvitationOnly.ToString(),
                 TeamMembers = new List<TeamMember> { new TeamMember { PageShortName = "api-test-c166ab39-fc80-4cf1-aed0-9236974740ca" } }
 			};
+            var teamId = teamResources.CreateOrUpdate(team1).Id;
 
-			var teamId = _api.CreateOrUpdate(team1).Id;
+            //act
+            var team2 = teamResources.Retrieve(team1.TeamShortName);
 
-			var team2 = _api.Retrieve(team1.TeamShortName);
-
+            //assert
 			Assert.That(team2.Id, Is.EqualTo(teamId));
 		}
 
-    	private void WithAValidClient(WireDataFormat format = WireDataFormat.Xml)
-    	{
-    		_client = TestContext.CreateClientValidCredentials(format);
-    		_api = new TeamApi(_client.HttpChannel);
-    	}
+        private static RegisterPageRequest ValidRegisterPageRequest()
+        {
+            return new RegisterPageRequest
+            {
+                ActivityType = null,
+                PageShortName = "test-frp-" + Guid.NewGuid(),
+                PageTitle =
+                    "When Provided With Valid Authentication Details And An Empty Activity Type - Creates New Page",
+                EventName = "The Other Occasion of ApTest and APITest",
+                CharityId = 2050,
+                EventId = TestConfigurationsHelper.GetProperty<ITestConfigurations, int>(x => x.ValidEventId),
+                TargetAmount = 20M,
+                EventDate = DateTime.Now.AddDays(5)
+            };
+        }
+
+        private static Team ValidTeamRequest(string pageShortName)
+        {
+            return new Team
+                {
+                    Name = "My Awesome event",
+                    Target = 1000.00m,
+                    Story = "My kick ass story",
+                    TargetType = TeamTargetType.Fixed.ToString(),
+                    TeamShortName = "my-great-team" + Guid.NewGuid(),
+                    TeamType = TeamType.Open.ToString(),
+                    TeamMembers =
+                        new List<TeamMember>
+                            {
+                                new TeamMember {PageShortName = pageShortName}
+                            }
+                };
+        }
     }
 }
