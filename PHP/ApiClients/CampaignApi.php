eb96dbf7c1906e1fcd5f46a9e480493e2a0b4220
pawelsawicz <pawel.sawicz@justgiving.com>
2016-02-24 14:09:42 +0000

    Update Campaigns, add V2 implementation

--- PHP/ApiClients/CampaignApi.php
+++ PHP/ApiClients/CampaignApi.php
@@ -24,6 +24,17 @@ class CampaignApi extends ClientBase
 		return json_decode($json);
 	}
 
+	public function RetrieveV2($charityName, $campaignName)
+	{
+		$httpResponse = new HTTPResponse();
+		$locationFormat = $this->Parent->RootDomain . "{apiKey}/v{apiVersion}/campaigns/". $charityName ."/". $campaignName;
+		$url = $this->BuildUrl($locationFormat);
+		$result = $this->curlWrapper->GetV2($url, $this->BuildAuthenticationValue());
+		$httpResponse->bodyResponse = json_decode($result->bodyResponse);
+		$httpResponse->httpStatusCode = $result->httpStatusCode;	
+		return $httpResponse;
+	}
+
 	public function Create($campaignCreationRequest)
 	{
 		$locationFormat = $this->Parent->RootDomain . "{apiKey}/v{apiVersion}/campaigns";
@@ -33,4 +44,26 @@ class CampaignApi extends ClientBase
 		return json_decode($json);
 	}
 
+	public function CreateV2($campaignCreationRequest)
+	{
+		$httpResponse = new HTTPResponse();
+		$locationFormat = $this->Parent->RootDomain . "{apiKey}/v{apiVersion}/campaigns";
+		$url = $this->BuildUrl($locationFormat);
+		$payload = json_encode($campaignCreationRequest);
+		$result = $this->curlWrapper->PutV2($url, $this->BuildAuthenticationValue(), $payload);
+		$httpResponse->bodyResponse = json_decode($result->bodyResponse);
+		$httpResponse->httpStatusCode = $result->httpStatusCode;	
+		return $httpResponse;
+	}
+
+	public function PagesForCampaign($charityShortName, $campaignShortUrl)
+	{
+		$httpResponse = new HTTPResponse();
+		$locationFormat = $this->Parent->RootDomain . "{apiKey}/v{apiVersion}/campaigns/". $charityShortName ."/". $campaignShortUrl . "/pages";
+		$url = $this->BuildUrl($locationFormat);
+		$result = $this->curlWrapper->GetV2($url, $this->BuildAuthenticationValue());
+		$httpResponse->bodyResponse = json_decode($result->bodyResponse);
+		$httpResponse->httpStatusCode = $result->httpStatusCode;	
+		return $httpResponse;
+	}
 }
