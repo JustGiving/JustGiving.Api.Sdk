1f99cc3ae98ec91533a33856899ef512feee42e6
Luis Pereira <Luis.Pereira@blackbaud.me>
2019-08-19 21:40:33 +0100

    [LP] Fix build and push pipelines

--- DotNet/src/create-rc-package.bat
+++ DotNet/src/create-rc-package.bat
@@ -1,2 +1,3 @@
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Release "JustGiving.Api.Sdk.sln"
-.nuget\nuget pack justgiving-sdk-rc.1.6.9.nuspec
+.nuget\nuget pack justgiving-sdk-rc.1.6.10.nuspec
+.nuget\nuget push justgiving-sdk-rc.1.6.10.nupkg %NugetApiKey% -Source https://api.nuget.org/v3/index.json
