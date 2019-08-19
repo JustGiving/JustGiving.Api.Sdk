1016451398dddb34a779edf2d60041a97961f355
Luis Pereira <Luis.Pereira@blackbaud.me>
2019-08-19 21:29:08 +0100

    [LP] Fixing create-rc-package.bat and adding nuspec file

--- DotNet/src/create-rc-package.bat
+++ DotNet/src/create-rc-package.bat
@@ -1,2 +1,2 @@
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Release "JustGiving.Api.Sdk.sln"
-.nuget\nuget pack "justgiving-sdk-rc.1.6.6.nuspec"
+.nuget\nuget pack justgiving-sdk-rc.1.6.9.nuspec
