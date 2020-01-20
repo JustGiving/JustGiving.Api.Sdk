5525e42bcdeba6c20516ee80282920ddef60c6d2
Luis Pereira <Luis.Pereira@blackbaud.me>
2020-01-20 22:11:21 +0000

    [LP] DIG-24 Fix build

--- DotNet/src/create-rc-package.bat
+++ DotNet/src/create-rc-package.bat
@@ -1,3 +1,3 @@
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Release "JustGiving.Api.Sdk.sln"
-.nuget\nuget pack justgiving-sdk-rc.1.6.12.nuspec
-.nuget\nuget push justgiving-sdk-rc.1.6.12.nupkg %NugetApiKey% -Source https://api.nuget.org/v3/index.json
+.nuget\nuget pack justgiving-sdk-rc.1.6.13.nuspec
+.nuget\nuget push justgiving-sdk-rc.1.6.13.nupkg %NugetApiKey% -Source https://api.nuget.org/v3/index.json
